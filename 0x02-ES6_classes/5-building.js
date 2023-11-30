a2ccd155844c7f01ec0a8f9714e057e39847a6f2
JOSEPH ANI <60388143+Nonso024@users.noreply.github.com>
2023-11-30 20:39:39 +0100

    mandatory tasks

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+  }
+
+  get() { return this.sqft; }
+
+  set sqft(feet) {
+    this._sqft = feet;
+  }
+}
